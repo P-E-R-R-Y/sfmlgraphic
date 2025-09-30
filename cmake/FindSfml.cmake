22e126616b3bda6abf49dc9203cc70be74666485
Chouteau Perry <perry.chouteau@epitech.eu>
2025-09-30 13:34:01 +0200

    fix(cmake): FindSfml -> add fpic

--- cmake/FindSfml.cmake
+++ cmake/FindSfml.cmake
@@ -10,5 +10,9 @@ if (NOT ${name}_FOUND) # If there's none, fetch and build raylib
     GIT_REPOSITORY "https://github.com/SFML/SFML.git"
     GIT_TAG        "${SFML_VERSION}"
   )
+
+  set(BUILD_SHARED_LIBS OFF CACHE BOOL "Build static libs" FORCE)
+  set(CMAKE_POSITION_INDEPENDENT_CODE ON CACHE BOOL "Enable PIC for static libs" FORCE)
+
   FetchContent_MakeAvailable(${name})
 endif()
